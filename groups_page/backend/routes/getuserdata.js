14d6e1122e847e8d1d6672e22bb4f4dad858ea2b
Gowtham-1901 <gowthamravi1904@gmail.com>
2023-10-11 11:58:09 +0530

    corrected

--- groups_page/backend/routes/getuserdata.js
+++ groups_page/backend/routes/getuserdata.js
@@ -6,7 +6,7 @@ const pool = require("../config/db");
 
 // getting all the datas
 router.get('/getuserdata', async (req, res) => {
-    const result = await pool.query("SELECT * FROM user_table JOIN role ON user_table.role_id= role.role_id WHERE isactive = true");
+    const result = await pool.query("SELECT * FROM user_table JOIN role ON user_table.role_id= role.role_id WHERE isactive = true AND (role.role_id = '1' OR role.role_id = '2')");
     // console.log(result);
     res.status(200).json(result.rows);
 });
